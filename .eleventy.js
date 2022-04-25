803c272b864b4b5133254ae53020407ddc4897db
Anton Volkov <akronbolkov@gmail.com>
2022-04-25 14:55:49 +0400

    Add mobile menu

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,39 @@
+const htmlnano = require("htmlnano");
+
 module.exports = function (config) {
   config.addPassthroughCopy("src/assets");
+
+  config.addTransform("html-min", (content, outputPath) => {
+    if (outputPath) {
+      const isHtml = outputPath.endsWith(".html");
+
+      if (isHtml) {
+        return htmlnano
+          .process(content, {
+            collapseAttributeWhitespace: true,
+            collapseWhitespace: "conservative",
+            deduplicateAttributeValues: false,
+            minifyCss: false,
+            minifyJs: true,
+            minifyJson: false,
+            minifySvg: false,
+            removeComments: "safe",
+            removeEmptyAttributes: false,
+            removeAttributeQuotes: false,
+            removeRedundantAttributes: true,
+            sortAttributesWithLists: false,
+            removeOptionalTags: false,
+            collapseBooleanAttributes: true,
+            mergeStyles: false,
+            mergeScripts: false,
+            minifyUrls: false,
+          })
+          .then((result) => result.html);
+      }
+    }
+
+    return content;
+  });
 
   return {
     dir: {
