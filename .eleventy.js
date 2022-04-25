38f020f8230a2971b140184ab2a9b22a8a62794a
Anton Volkov <akronbolkov@gmail.com>
2022-04-25 19:27:03 +0400

    Add footer

--- .eleventy.js
+++ .eleventy.js
@@ -1,39 +1,45 @@
 const htmlnano = require("htmlnano");
 
+const isProduction = process.env.NODE_ENV === "production";
+
 module.exports = function (config) {
+  config.setUseGitIgnore(false);
+
   config.addPassthroughCopy("src/assets");
 
-  config.addTransform("html-min", (content, outputPath) => {
-    if (outputPath) {
-      const isHtml = outputPath.endsWith(".html");
+  if (isProduction) {
+    config.addTransform("html-min", (content, outputPath) => {
+      if (outputPath) {
+        const isHtml = outputPath.endsWith(".html");
 
-      if (isHtml) {
-        return htmlnano
-          .process(content, {
-            collapseAttributeWhitespace: true,
-            collapseWhitespace: "conservative",
-            deduplicateAttributeValues: false,
-            minifyCss: false,
-            minifyJs: true,
-            minifyJson: false,
-            minifySvg: false,
-            removeComments: "safe",
-            removeEmptyAttributes: false,
-            removeAttributeQuotes: false,
-            removeRedundantAttributes: true,
-            sortAttributesWithLists: false,
-            removeOptionalTags: false,
-            collapseBooleanAttributes: true,
-            mergeStyles: false,
-            mergeScripts: false,
-            minifyUrls: false,
-          })
-          .then((result) => result.html);
+        if (isHtml) {
+          return htmlnano
+            .process(content, {
+              collapseAttributeWhitespace: true,
+              collapseWhitespace: "conservative",
+              deduplicateAttributeValues: false,
+              minifyCss: false,
+              minifyJs: true,
+              minifyJson: false,
+              minifySvg: false,
+              removeComments: "safe",
+              removeEmptyAttributes: false,
+              removeAttributeQuotes: false,
+              removeRedundantAttributes: true,
+              sortAttributesWithLists: false,
+              removeOptionalTags: false,
+              collapseBooleanAttributes: true,
+              mergeStyles: false,
+              mergeScripts: false,
+              minifyUrls: false,
+            })
+            .then((result) => result.html);
+        }
       }
-    }
 
-    return content;
-  });
+      return content;
+    });
+  }
 
   return {
     dir: {
